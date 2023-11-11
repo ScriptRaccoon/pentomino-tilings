003e1b181889a7134ec011b628e6bf6f746e6ebf
Script Raccoon <scriptraccoon@gmail.com>
2023-11-11 18:57:05 +0100

    make n,m scoped variables

--- scripts/pentomino_tilings.py
+++ scripts/pentomino_tilings.py
@@ -14,43 +14,25 @@ from exactcover import exactcovers
 import utils
 
 
-n, m = 4, 15
-"""Dimension of the rectangle"""
-
-assert n >= 3 and m >= 3 and n * m == 60
-
-COORDINATES = {(i, j) for i in range(n) for j in range(m)}
-"""Set of coordinates of the rectangle"""
-
-
-def get_constraints() -> set[str]:
+def get_choices(n: int, m: int) -> Iterator[set[str]]:
     """
-    The constraints are the coordinates (i,j) of the rectangle,
-    encoded as strings i|j, as well as the names of the pentominoes.
-    """
-    return {f"{i}|{j}" for i, j in COORDINATES}.union(
-        {p.name for p in LIST_PENTOMINOES}
-    )
-
-
-def get_choices() -> Iterator[set[str]]:
-    """
-    The choices are the sets {N, i|j, ...} where N is the name of a
-    pentomino, potentially rotated and/or reflected, and i|j,...
+    The choices are the sets {N, i|j, u|v, ...} where N is the name of a
+    pentomino, potentially rotated and/or reflected, and (i,j), (u,v),...
     are its coordinates, as long as they fit into the rectangle.
     """
     for p in LIST_PENTOMINOES:
         for q in p.variations():
-            for i, j in COORDINATES:
-                if utils.valid_shift(q.coords, (i, j), (n, m)):
-                    yield {f"{y+i}|{x+j}" for y, x in q.coords}.union({p.name})
+            for i in range(n):
+                for j in range(m):
+                    if utils.valid_shift(q.coords, (i, j), (n, m)):
+                        yield {f"{y+i}|{x+j}" for y, x in q.coords}.union({p.name})
 
 
 def transform_tiling(tiling: list[set[str]]) -> dict[str, list[tuple[int, int]]]:
     """
     The exact cover algorithm returns a list of sets, each one having the form
-    {N, i|j, ...}. We transform such a tiling to a dictionary with entries
-    N : {(i,j),...}
+    {N, i|j, u|v, ...}. We transform such a tiling to a dictionary with entries
+    N : {(i,j), (u,v), ...}
     """
     transform: dict[str, list[tuple[int, int]]] = {}
     for tile in tiling:
@@ -71,18 +53,28 @@ def transform_tiling(tiling: list[set[str]]) -> dict[str, list[tuple[int, int]]]
     return transform
 
 
-def get_tilings() -> Iterator[dict[str, list[tuple[int, int]]]]:
+def get_tilings(n: int, m: int) -> Iterator[dict[str, list[tuple[int, int]]]]:
     """
     Generator containing all pentomino tilings of a rectangle of size n x m
     where each pentomino name appears exactly once. They are determined using
     the exact cover algorithm.
     """
-    X = get_constraints()
-    S = list(get_choices())
+    if not (isinstance(n, int) and isinstance(m, int)):
+        raise TypeError("Sizes need to be integers")
+    if not (n >= 3 and m >= 3 and n * m == 60):
+        raise ValueError("Invalid size")
+
+    X = {f"{i}|{j}" for i in range(n) for j in range(m)}.union(
+        {p.name for p in LIST_PENTOMINOES}
+    )
+    S = list(get_choices(n, m))
+
     return map(transform_tiling, exactcovers(X, S))
 
 
-def print_tiling_to_console(tiling: dict[str, list[tuple[int, int]]]) -> None:
+def print_tiling_to_console(
+    n: int, m: int, tiling: dict[str, list[tuple[int, int]]]
+) -> None:
     """Prints a tiling to the console, where each pentomino is printed with its name"""
     board = [["" for _ in range(m)] for _ in range(n)]
     for name, coords in tiling.items():
@@ -93,20 +85,20 @@ def print_tiling_to_console(tiling: dict[str, list[tuple[int, int]]]) -> None:
     print()
 
 
-def print_tilings() -> None:
+def print_tilings(n: int, m: int) -> None:
     """Prints each tiling and waits for input to generate the next one"""
-    for tiling in get_tilings():
-        print_tiling_to_console(tiling)
+    for tiling in get_tilings(n, m):
+        print_tiling_to_console(n, m, tiling)
         input()
 
 
-def save_tilings() -> None:
+def save_tilings(n: int, m: int) -> None:
     """Compute the list of all tilings (takes a while) and saves them as JSON file"""
-    tilings = list(get_tilings())
+    tilings = list(get_tilings(n, m))
     with open(f"../public/data/tilings-{n}-{m}.json", "w", encoding="utf8") as file:
         json.dump(tilings, file)
 
 
 if __name__ == "__main__":
-    # print_tilings()
-    save_tilings()
+    # print_tilings(4, 15)
+    save_tilings(3, 20)
