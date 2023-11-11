903f608b62bca40d0edbca1301b19370e30e9582
Script Raccoon <scriptraccoon@gmail.com>
2023-11-11 03:00:55 +0100

    improve styling

--- src/App.svelte
+++ src/App.svelte
@@ -52,52 +52,110 @@
 	onMount(init)
 </script>
 
-<h1>Pentomino Tilings</h1>
+<header>
+	<h1>Pentomino Tilings</h1>
+</header>
 
-<menu>
-	<button disabled={!current_tiling} on:click={go_left}>
-		Left
-	</button>
-	<button disabled={!current_tiling} on:click={go_right}>
-		Right
-	</button>
-	<label for="size">Size</label>
-	<select id="size" bind:value={selected_height} on:change={init}>
-		{#each Object.entries(SIZES) as [value, label]}
-			<option {value}>{@html label}</option>
-		{/each}
-	</select>
-</menu>
-
-{#if current_tiling}
-	<p>
-		Tiling #{current_index + 1}
-	</p>
-	<div class="board" style:--n={n} style:--m={m}>
-		{#each names as name}
-			{#each { length: 5 } as _, index}
-				{@const [y, x] = current_tiling[name][index]}
-				<div
-					class="cell"
-					style:--x={x}
-					style:--y={y}
-					style:--color={COLORS[name]}
+<main>
+	<menu>
+		<div>
+			<button
+				disabled={!current_tiling}
+				on:click={go_left}
+				aria-label="previous"
+			>
+				<img
+					src="arrow.svg"
+					alt="arrow left"
+					aria-hidden="true"
 				/>
-			{/each}
-		{/each}
-	</div>
-{/if}
+			</button>
+			<button
+				disabled={!current_tiling}
+				on:click={go_right}
+				aria-label="next"
+			>
+				<img
+					class="flipped"
+					src="arrow.svg"
+					alt="arrow right"
+					aria-hidden="true"
+				/>
+			</button>
+		</div>
 
-{#if loading}
-	<div>Loading...</div>
-{/if}
+		<div>
+			<label for="size">Size</label>
+			<select
+				id="size"
+				bind:value={selected_height}
+				on:change={init}
+				disabled={loading}
+			>
+				{#each Object.entries(SIZES) as [value, label]}
+					<option {value}>{@html label}</option>
+				{/each}
+			</select>
+		</div>
+	</menu>
+
+	{#if current_tiling}
+		<h2 class="counter" aria-live="polite">
+			Tiling #{current_index + 1}
+		</h2>
+		<div class="board" style:--n={n} style:--m={m}>
+			{#each names as name}
+				{#each { length: 5 } as _, index}
+					{@const [y, x] = current_tiling[name][index]}
+					<div
+						class="cell"
+						style:--x={x}
+						style:--y={y}
+						style:--color={COLORS[name]}
+					/>
+				{/each}
+			{/each}
+		</div>
+	{/if}
+
+	{#if loading}
+		<div>Loading...</div>
+	{/if}
+</main>
 
 <style>
+	header {
+		padding-block: 2rem;
+	}
+
+	h1 {
+		line-height: 1.1;
+	}
+
+	menu {
+		display: flex;
+		align-items: center;
+		gap: 1.5rem;
+	}
+
+	menu div {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	.counter {
+		font-size: 1.25rem;
+		font-weight: initial;
+		margin-block: 1rem 0.5rem;
+	}
+
 	.board {
-		--u: 3rem;
+		--u: min(5rem, calc((100vw - 2rem) / var(--m)));
 		position: relative;
 		height: calc(var(--n) * var(--u));
 		width: calc(var(--m) * var(--u));
+		margin-block: 0.5rem;
 	}
 
 	.cell {
@@ -106,8 +164,23 @@
 		height: var(--u);
 		left: calc(var(--x) * var(--u));
 		top: calc(var(--y) * var(--u));
-		border: 1px solid gray;
+		border: 1px solid var(--bg);
 		position: absolute;
 		transition: 750ms ease;
+	}
+
+	button {
+		opacity: 0.75;
+	}
+
+	button:hover,
+	button:focus-visible {
+		opacity: 1;
+	}
+
+	@media (min-width: 42rem) {
+		h1 {
+			font-size: 3rem;
+		}
 	}
 </style>
